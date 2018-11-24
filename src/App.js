486636e542d63d390015a271d9284942bf630d45
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-11-23 18:19:48 -0600

    import Board component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 
-import List from "./Components/List/List"
+// import List from "./Components/List/List"
+import Board from "./Components/Board/Board";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <List />
+        <Board />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
